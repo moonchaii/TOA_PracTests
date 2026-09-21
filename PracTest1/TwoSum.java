d88484cd77826174a44009712a4dde415fa20904
Chai Europa <teuropa123@gmail.com>
2026-09-21 15:45:47 +0200

    two sum brute force

---
+++ PracTest1/TwoSum.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        int target = scanner.nextInt();
+
+        int[] result = twoSum(nums, target);
+        if (result.length == 2) {
+            System.out.println(Arrays.toString(result).replace(" ", "")); 
+        } else {
+            System.out.println("No solution found.");
+        }
+        
+        scanner.close();
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
