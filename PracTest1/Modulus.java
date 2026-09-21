e4ac55918b493fc326ffdfd532c964bb447a88b5
Chai Europa <teuropa123@gmail.com>
2026-09-21 13:28:01 +0200

    first brute force algorithm practice

---
+++ PracTest1/Modulus.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Modulus {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n, z;
+        n = scanner.nextInt();
+        z = scanner.nextInt();
+
+        int count = 0;
+
+        for (int x = 1; x < n; x++) {
+            for (int y = 1; y < n; y++) {
+                if ((x * y) % n == z) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
